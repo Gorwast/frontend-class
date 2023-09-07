5dc98406b83d648ce4c420895bda1edb1e27fb08
Gorwast <lk23@live.com.mx>
2023-09-06 17:21:12 -0700

    Ej 1. SITIO WEB ESTÁTICO CON VANILLA JS

--- js/app.js
+++ js/app.js
@@ -7,3 +7,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
         const nombreconst = contactoForm.getElementById('nombre').value();
     })
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ScrollToBottomButton = document.getElementById("btn-ir-arriba");
+    
+  
+    window.addEventListener("scroll", () => {
+      
+      if (window.scrollY > 100) {
+          ScrollToBottomButton.style.display = 'block';
+      } else {
+          ScrollToBottomButton.style.display = 'none';
+      }
+    })
+  
+    ScrollToBottomButton.addEventListener('click', ()=> {
+      window.scrollTo(top)
+    })
+  });
