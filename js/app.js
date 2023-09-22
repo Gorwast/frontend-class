859a8fbc911ea88a69f33509845e7de5b9fc2295
Gorwast <lk23@live.com.mx>
2023-09-21 18:02:56 -0700

    Ej 4 migracion a sass

--- js/app.js
+++ js/app.js
@@ -1,27 +1,41 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const contactoForm = document.getElementById('contacto-form');
+document.addEventListener("DOMContentLoaded", () => {
+  const contactoForm = document.getElementById("contacto-form");
 
-    contactoForm.addEventListener("submit", (event) => {
-        event.preventDefault();
+  contactoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        const nombreconst = contactoForm.getElementById('nombre').value();
-    })
-})
+    const nombreconst = contactoForm.getElementById("nombre").value();
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
-    const ScrollToBottomButton = document.getElementById("btn-ir-arriba");
-    
-  
-    window.addEventListener("scroll", () => {
-      
-      if (window.scrollY > 100) {
-          ScrollToBottomButton.style.display = 'block';
-      } else {
-          ScrollToBottomButton.style.display = 'none';
-      }
-    })
-  
-    ScrollToBottomButton.addEventListener('click', ()=> {
-      window.scrollTo(top)
-    })
+  const ScrollToBottomButton = document.getElementById("btn-ir-arriba");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      ScrollToBottomButton.style.display = "block";
+    } else {
+      ScrollToBottomButton.style.display = "none";
+    }
   });
+
+  ScrollToBottomButton.addEventListener("click", () => {
+    window.scrollTo(top);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ScrollToBottomButton = document.getElementById("btn-ir-arriba");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      ScrollToBottomButton.style.display = "block";
+    } else {
+      ScrollToBottomButton.style.display = "none";
+    }
+  });
+
+  ScrollToBottomButton.addEventListener("click", () => {
+    window.scrollTo(top);
+  });
+});
